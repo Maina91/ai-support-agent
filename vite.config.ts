4441633fd541b06413962467c63c05e113203fd9
Maina <mainaedwin913@gmail.com>
2025-07-11 06:45:38 +0300

    configured shadcn

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 import tailwindcss from '@tailwindcss/vite';
+import tsconfigPaths from "vite-tsconfig-paths";
 import path from "path";
 
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   root: ".",
   publicDir: "public",
   server: {
